99aa8f392aac4dc06f8277912cb1397ca57f3fbf
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-07-14 21:43:45 +0530

    Create fizzbuzz.c

---
+++ fizzbuzz.c
@@ -0,0 +1,31 @@
+char ** fizzBuzz(int n, int* returnSize){
+
+    int i;
+    
+    *returnSize = n;
+    char **str = (char**)malloc(sizeof(char*)*n);
+    for (int i = 0; i < n; i++) {
+       str[i] = (char*)malloc(sizeof(char) * 9);
+	}
+
+    for(i = 1;i<=n;i++)
+    {
+        if(i % 3 == 0 && i % 5 == 0)
+        {
+            strcpy(&str[i - 1][0],"FizzBuzz");
+        }
+        else if(i % 3 == 0)
+        {
+            strcpy(&str[i-1][0],"Fizz");
+        }
+        else if(i % 5 == 0)
+        {
+            strcpy(&str[i-1][0], "Buzz");
+        }
+        else 
+        {
+            sprintf(&str[i-1][0] ,"%i",i);
+        }
+    }
+    return &str[0];
+}
